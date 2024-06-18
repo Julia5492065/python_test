91e7bc4f8e009657fb4e0518c6104ccb97636d39
juliaT <julia5492065@gmail.com>
2024-06-19 01:06:05 +0800

    fix first ans

--- 1_10/d827_operation.py
+++ 1_10/d827_operation.py
@@ -1,5 +1,8 @@
-from math import floor # 無條件捨去
+from math import floor
 total=0
 n=int(input())
-total=floor(n/12)*50+(n%12)*5
+if n % 12 ==0:
+    total=floor(n/12)*50
+else :
+    total=floor(n/12)*50+(n%12)*5
 print(total)
