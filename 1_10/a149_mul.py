8ee6c468d4034b7344f72c69c9790454b1d3cb78
juliaT <julia5492065@gmail.com>
2024-07-11 20:56:07 +0800

    simple_ans

--- 1_10/a149_mul.py
+++ 1_10/a149_mul.py
@@ -1,11 +1,12 @@
 t=int(input("測試數量"))
 while t!=0:
     m=input("輸入值")
-    x=[]
-    for a in str(m):
-        x.insert(0,a)
+    x=[int(a) for a in m]
+    # x=[]
+    # for a in str(m):
+    #     x.insert(0,a)
     y=1
     for b in x:
-        y*=int(b)
-        print(y)
+        y*=b
+    print(y)
     t-=1
