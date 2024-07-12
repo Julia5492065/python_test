32130df98c71a23cea77cbde71457ce8f6b5c620
juliaT <julia5492065@gmail.com>
2024-07-12 20:36:45 +0800

    1.刪除了多餘的 a=[ ] 2.刪除了 if i == t : break 3.簡化條件檢查和計算過程 4.增加一個情況，會提示「無法確定序列類型」。

--- 1_10/a005.py
+++ 1_10/a005.py
@@ -1,16 +1,16 @@
 try:
     t=int(input("請輸入數量"))
-    a=[]
     if 0<=t<=20:
-        for i in range(0,t):
+        for i in range(t):
             a=list(map(int,input().split()))
             if len(a)==4:
                 if a[1]-a[0]==a[2]-a[1]==a[3]-a[2] :
-                    a.append(int(a[3]+(a[2]-a[1])))
-                    print(" ".join(map(str, a)))
+                    a.append(a[3]+(a[2]-a[1]))
                 elif a[1]/a[0]==a[2]/a[1]==a[3]/a[2]:
                     a.append(int(a[3]*(a[2]/a[1])))
-                    print(" ".join(map(str, a)))
-            if i == t : break
+                else:
+                    print("無法確定序列類型")
+                    continue
+                print(" ".join(map(str, a)))
 except ValueError:
     print("請重新輸入")
